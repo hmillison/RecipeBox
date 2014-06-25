9648504472f56f3e8b4937770a478979a07582a2
Henry Millison <hmillison@gmail.com>
2014-06-25 16:27:48 -0400

    Scrapes foodnetwork urls based on user input, outputs result

--- app/routes.js
+++ app/routes.js
@@ -1,33 +1,75 @@
 // app/routes.js
+module.exports = function(app, passport) {
 
-	module.exports = function(app) {
+	// =====================================
+	// HOME PAGE (with login links) ========
+	// =====================================
+	app.get('/', function(req, res) {
+		res.render('index.ejs',{recipes: ""}); // load the index.ejs file
+	});
+	
+	
 
-		// server routes ===========================================================
-		// handle things like api calls
-		// authentication routes
+	// =====================================
+	// LOGIN ===============================
+	// =====================================
+	// show the login form
+	app.get('/login', function(req, res) {
 
-		// sample api route
-		app.get('/api/nerds', function(req, res) {
-			// use mongoose to get all nerds in the database
-			Nerd.find(function(err, nerds) {
+		// render the page and pass in any flash data if it exists
+		res.render('login.ejs', { message: req.flash('loginMessage') }); 
+	});
 
-				// if there is an error retrieving, send the error. nothing after res.send(err) will execute
-				if (err)
-					res.send(err);
+	// process the login form
+	// app.post('/login', do all our passport stuff here);
 
-				res.json(nerds); // return all nerds in JSON format
-			});
+	// =====================================
+	// SIGNUP ==============================
+	// =====================================
+	// show the signup form
+	app.get('/signup', function(req, res) {
+
+		// render the page and pass in any flash data if it exists
+		res.render('signup.ejs', { message: req.flash('signupMessage') });
+	});
+
+	// process the signup form
+	// app.post('/signup', do all our passport stuff here);
+
+	// =====================================
+	// PROFILE SECTION =====================
+	// =====================================
+	// we will want this protected so you have to be logged in to visit
+	// we will use route middleware to verify this (the isLoggedIn function)
+	app.get('/profile', isLoggedIn, function(req, res) {
+		res.render('profile.ejs', {
+			user : req.user // get the user out of session and pass to template
 		});
+	});
 
-		// route to handle creating (app.post)
-		app.set('port', process.env.PORT || 8081);
+	// =====================================
+	// LOGOUT ==============================
+	// =====================================
+	app.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
+	
+	// process the signup form
+	app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/profile', // redirect to the secure profile section
+		failureRedirect : '/signup', // redirect back to the signup page if there is an error
+		failureFlash : true // allow flash messages
+	}));
+};
 
-		// route to handle delete (app.delete)
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
 
-		// frontend routes =========================================================
-		// route to handle all angular requests
-		app.get('*', function(req, res) {
-			res.sendfile('./public/index.html'); // load our public/index.html file
-		});
+	// if user is authenticated in the session, carry on 
+	if (req.isAuthenticated())
+		return next();
 
-	};
+	// if they aren't redirect them to the home page
+	res.redirect('/');
+}
--- server.js
+++ server.js
@@ -5,7 +5,14 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 var mongoose = require('mongoose'); //MongoDB
+var passport = require('passport');
+var flash 	 = require('connect-flash');
 
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+app.use(bodyParser());
 
 // configuration ===========================================
 		
@@ -13,33 +20,51 @@ var mongoose = require('mongoose'); //MongoDB
 	var db = require('./config/db');
 
 	var port = process.env.PORT || 8080; // set our port
-	// mongoose.connect(db.url); // connect to our mongoDB database (uncomment after you enter in your own credentials in config/db.js)
+	mongoose.connect(db.url); // connect to our mongoDB database
+	
+     app.use(express.static(__dirname + '../public'));
+ 
+		
+       // require('./config/passport')(passport); // pass passport for configuration
 
-	app.configure(function() {
-		app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
-		app.use(express.logger('dev')); 					// log every request to the console
-		app.use(express.bodyParser()); 						// have the ability to pull information from html in POST
-		app.use(express.methodOverride()); 					// have the ability to simulate DELETE and PUT
-		app.use(express.json());       // to support JSON-encoded bodies
-		app.use(express.urlencoded()); // to support URL-encoded bodies
-
-	});
-
+		// set up our express application
+		app.use(morgan('dev')); // log every request to the console
+		app.use(cookieParser()); // read cookies (needed for auth)
+		app.set('view engine', 'ejs'); // set up ejs for templating
+		// required for passport
+		app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+		app.use(passport.initialize());
+		app.use(passport.session()); // persistent login sessions
+		app.use(flash()); // use connect-flash for flash messages stored in session
 	// routes ==================================================
 
+	require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+var posturl = ""
+// This route receives the posted form.
+// As explained above, usage of 'body-parser' means
+// that `req.body` will be filled in with the form elements
+app.post('/', function(req, res){
+	 posturl = req.body.url;
+  res.redirect('/scrape');
+
+});
 
 
 //=====web scraper for Food Network recipes===
 app.get('/scrape', function(req, res){
-	url = 'http://www.foodnetwork.com/recipes/bobby-deen/bobbys-loaded-baked-potato-recipe.html?ic1=obinsite';
 
+	url = posturl;
+
+ 	console.log(url);
 	request(url, function(error, response, html){
 		if(!error){
 			var $ = cheerio.load(html);
 
 			var food, ingredients, directions, image;
 			var json = { name : "", image: "", ingredients : "", directions : ""};
-
+	
+			//Get name of food item
 			$('.tier-3.title').filter(function(){
 		        var data = $(this);
 		        name = data.children().first().text();
@@ -47,6 +72,14 @@ app.get('/scrape', function(req, res){
 		        json.name = name;
 	        })
 
+			 //Get food image url
+	        $('.col12.pic.collapsed').filter(function(){
+	        	var data = $(this).find('img');
+	        	image = data.attr('src');
+	        	json.image = image;	
+	        })
+	        
+			//Get ingredients separated by ::
 	        $('.col6.ingredients').filter(function(){
 	        	var data = $(this);
 	        	var list = data.children().last().children();
@@ -59,15 +92,11 @@ app.get('/scrape', function(req, res){
 	        	json.ingredients = ingredients;
 	        })
 	        
-	        $('.col12.pic.collapsed').filter(function(){
-	        	var data = $(this).find('img');
-	        	image = data.attr('src');
-	        	json.image = image;	
-	        })
-	        
+	       
+	        //Get directions separated by ::
 	        $('.col12.directions').filter(function(){
 	        	var data = $(this);
-	        	var list = data.children().nextUntil('categories');
+	        	var list = data.children().nextUntil($('.categories'));
 	        	var parts = [];
 	        	directions = ""
 	        	list.each(function(i,elem){
@@ -76,13 +105,18 @@ app.get('/scrape', function(req, res){
 	        	});
 	        	json.directions = directions;	
 	        })
-		}
-
-		fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+	        fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
         	console.log('File successfully written! - Check your project directory for the output.json file');
         })
-
-        res.send('Check your console!')
+	 	var html = json.name + '<br> <img src="' +  json.image + '" alt="food" height="150px"> <br>';	
+		}
+		else
+		{
+			html = "<div class='flashmessage'> ERROR LOADING RECIPE </div>";
+		}
+		
+		res.render('index.ejs',{recipes: html}); // load the index.ejs file
+        //res.write('Check your console!')
 	})
 })
 app.listen('8081');
