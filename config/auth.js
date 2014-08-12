57cd2dc139f52b7c16611421a05cbc394498ef2a
Henry Millison <hmillison@gmail.com>
2014-08-11 23:12:09 -0400

    update to callback url for heroku

--- config/auth.js
+++ config/auth.js
@@ -5,7 +5,7 @@ module.exports = {
 	'googleAuth' : {
 		'clientID' 		: '569455270100-r4897qqipsnut2ein6leq3c1qnb6bqi5.apps.googleusercontent.com',
 		'clientSecret' 	: 'DN2WPeQrTKQsFjzXG1HPiOBF',
-		'callbackURL' 	: 'http://localhost:8081/oauth2callback'
+		'callbackURL' 	: 'http://my-recipe-box.herokuapp.com/oauth2callback'
 	}
 
 };
